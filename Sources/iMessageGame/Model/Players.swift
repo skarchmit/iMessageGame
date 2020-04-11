c52bf29ea13704e571405555bc0c3976e602c36a
Sergey Karchmit <skarchmit@gmail.com>
2020-04-10 17:01:01 -0700

    Players: Potential randomization

--- Sources/iMessageGame/Model/Players.swift
+++ Sources/iMessageGame/Model/Players.swift
@@ -9,4 +9,18 @@ import Foundation
 
 public class Players {
 	
+	/// Randomizes the list of players
+	public func randomize() {
+		
+	}
+	
+	/// Get a random player
+	/// - Returns: returns a random player
+	/// - Parameter includeYourself: Should the ranom player be yourself?
+	public func random(includeYourself: Bool = false) -> Player {
+		// TODO: Return a random Player
+		// TODO: Check if the random player is included and return a different one
+		return Player(uuid: "")
+	}
+	
 }
