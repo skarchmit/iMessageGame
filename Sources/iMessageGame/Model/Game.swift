1f59f622ceeb9206e72cf1e0806b0947319d41d6
Sergey Karchmit <skarchmit@gmail.com>
2020-04-10 17:02:33 -0700

    Updated the game from MsMessage

--- Sources/iMessageGame/Model/Game.swift
+++ Sources/iMessageGame/Model/Game.swift
@@ -6,9 +6,22 @@
 //
 
 import Foundation
+import Messages
 
 public class Game {
 	
 	public var players: Players = Players()
+	
+}
+
+
+extension Game {
+	
+	/// Update the game from the message recieved
+	/// - Parameter message: The message used to update all the game Items
+	@available(iOS 10.0, *)
+	public func update(fromMessage message: MSMessage) {
+		
+	}
 	
 }
