5294e1709ea7cc33d15021a0b22ae0799f75f3af
Sergey Karchmit <skarchmit@gmail.com>
2020-04-28 00:56:18 -0700

    Scene Protocol needs some updates

--- Sources/iMessageGame/Model/Scene.swift
+++ Sources/iMessageGame/Model/Scene.swift
@@ -8,7 +8,8 @@
 import Foundation
 import SpriteKit
 
-public class Scene: SKScene {
+//@available(iOS 12.0, *)
+open class Scene: SKScene {
 	
 	public var game: GameProtocol
 	
@@ -17,8 +18,19 @@ public class Scene: SKScene {
 		super.init(size: size)
 	}
 	
-	required init?(coder aDecoder: NSCoder) {
+	required public init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
 	
+//	override public init(fileNamed: String) {
+//		super.init(fileNamed: fileNamed)!
+//	}
+//
+//	override init
+	
+}
+
+public protocol SceneProtocol {
+	var game: GameProtocol { get set }
+	func update()
 }
