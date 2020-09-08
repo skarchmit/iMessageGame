3dbd90cab50897627a800159c166c02537bdf957
Sergey Karchmit <skarchmit@gmail.com>
2020-09-07 23:12:34 -0700

    Update Scenes and SceneTypes and ScenesManageer

---
+++ Sources/iMessageGame/Model/SceneType.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Karchmit on 9/7/20.
+//
+
+import Foundation
+
+public enum SceneType{
+    case active, end, new, lobby
+}
---
+++ Sources/iMessageGame/Model/Scenes.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sergey Karchmit on 9/7/20.
+//
+
+import SpriteKit
+
+
+@available(iOS 12.0, *)
+open class Scenes {
+    
+    private var active, end, new, lobby: Scene?
+    public var current: Scene?
+    
+    public init(active: Scene? = nil, end: Scene? = nil, new: Scene? = nil, lobby: Scene? = nil) {
+        self.active = active
+        self.end = end
+        self.new = new
+        self.lobby = lobby
+    }
+    
+    public func requestScene(sceneType: SceneType) -> Scene {
+        switch sceneType {
+        case .active:
+            return self.active!
+        
+        case .end:
+            return self.end ?? self.new!
+            
+        default:
+            return self.new!
+        }
+    }
+    
+    
+}
