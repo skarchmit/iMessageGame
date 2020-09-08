109bdd39b0ab92ff73d21ecf5a46ff7404ccffab
Sergey Karchmit <skarchmit@gmail.com>
2020-09-07 23:12:04 -0700

    Deprecate SceneDelegate

--- Sources/iMessageGame/Model/SceneDelegate.swift
+++ Sources/iMessageGame/Model/SceneDelegate.swift
@@ -1,16 +1,49 @@
 //
-//  Scene.swift
+//  SceneLoader.swift
 //  
 //
-//  Created by Sergey Karchmit on 4/10/20.
+//  Created by Sergey Karchmit on 9/6/20.
 //
 
-import Foundation
-import SpriteKit
+//import SpriteKit
 //
-//@available(iOS 10.0, *)
-//public protocol SceneDelegate: SKScene {
-//    var observer: iMessageGame.MessagesVC? { get set }
+//@available(iOS 12.0, *)
+//open class SceneDelegate {
+//
+//    /// Delegate back to MessagesVC
+//    public weak var delegate: MessagesVC?
+//
+//    /// Scene Object that is being wrapped
+//    private var scene: Scene?
+//
+//    public init(fileNamed: String){
+//        self.scene = Scene(fileNamed: fileNamed)
+//        assignGameDelegate()
+//
+//    }
+//
+//    public init(skscene: SKScene){
+//        self.scene = skscene as? Scene
+//        assignGameDelegate()
+//
+//    }
+//
+//    public init(scene: Scene) {
+//        self.scene = scene
+//        assignGameDelegate()
+//
+//    }
+//
+//    func assignGameDelegate() {
+////        self.scene!.gameDelegate = self
+//    }
+////
+////    open func send(caption: String, summaryText: String, withConfirmation: Bool) {
+////        let message = composeMessage(caption: caption, summaryText: summaryText)
+////        delegate?.send(message: message!, withConfirmation: withConfirmation)
+////    }
+//
+//
+//
 //}
-//
-//
+
